d9f553117e7ffc23d66b3a19f32afe52fcd84523
Jashanpreet Singh <JS105@atcltd.atc.org.nz>
2019-05-29 10:02:58 +1200

    commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string actualpin = "0786";
+            int count = 0;
+            string pin;
+            do
+            {
+                pin = Console.ReadLine();
+                count++;
+            }
+            while (pin != actualpin && count < 3);
+            if (pin == actualpin)
+                Console.WriteLine("Welcome user");
+            else
+                Console.WriteLine("Account Locked");
+        }
+    }
+}
